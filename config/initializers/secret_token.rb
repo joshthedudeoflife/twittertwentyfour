b021caa5b44199f663a95ef1cf35f8820234baa9
josthedudeoflife <josh.chou@ses-technologies.com>
2014-04-07 13:24:23 -0400

    this is my GA project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twittertwentyfour::Application.config.secret_key_base = '26774b1b0c8349929baf15ed81d46076bd5347cfefe83c00c5fc6d8e6b65f62299581a4cbd2924d7e021990d4a8a4f4cd7d7db3afed60e51b1f5e1e44dd664cb'
